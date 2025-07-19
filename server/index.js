0b1ca4cdfca82ade9c8291cfd2491d5776007b16
Ali Khan <164453693+Alikhan23M@users.noreply.github.com>
2025-07-19 18:00:25 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://dockerized-cloudnotes-client.onrender.com'],
+    origin: ['https://dockerized-cloudnotes-client.onrender.com','*'],
     
   })
 );
